6bcf3daa8857af1670dc1d6a0b413ccb9b946b0b
Alex McHale <alex@anticlever.com>
2014-05-08 22:02:23 -0500

    Validate that a feed can be processed by Feedjira before saving.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -5,9 +5,18 @@ class Feed < ActiveRecord::Base
   validates :name, presence: true
   validates :url, presence: true
   validates :user_id, presence: true
+  validate :validate_feed_url
 
   def stories
     Feedjira::Feed.fetch_and_parse(url).entries
+  end
+
+  protected
+
+  def validate_feed_url
+    unless Feedjira::Feed.fetch_and_parse(url).respond_to? :entries
+      errors[:url] << "doesn't seem to be a valid rss feed"
+    end
   end
 
 end
