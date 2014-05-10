30c2f699029a8aa63712773992af531b4473c9ee
Alex McHale <alex@anticlever.com>
2014-05-09 19:59:43 -0500

    Start puma in production.

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # execute :touch, release_path.join('tmp/restart.txt')
       within release_path do
-        execute "cd #{release_path}; bin/puma.sh restart"
+        execute "cd #{release_path}; RAILS_ENV=production bin/puma.sh restart"
       end
     end
   end
